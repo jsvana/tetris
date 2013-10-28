d14721994e9bdc0d1199d9289d0757e15e370f1f
Jay Vana <jsvana@mtu.edu>
2013-10-28 19:37:54 -0400

    Initial commit

---
+++ ts/app.ts
@@ -0,0 +1,13 @@
+var init = function() {
+	var canvas = new fabric.Canvas('c');
+
+	var rect = new fabric.Rect({
+		left: 100,
+		top: 100,
+		fill: 'red',
+		width: 20,
+		height: 20
+	});
+
+	canvas.add(rect);
+};
